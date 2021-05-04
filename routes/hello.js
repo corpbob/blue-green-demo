076612358df9c964cc956c9868eda164c71eba90
corpbob <me@example.com>
2021-05-04 19:28:04 +0800

    convert to json response

--- routes/hello.js
+++ routes/hello.js
@@ -5,7 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   console.log("test2");
   var color = process.env.COLOR;
-  res.send('version 7: ' + color + '\n');
+  var the_response = { "color": color };
+  console.log("the response: ", the_response);
+  //res.send('version 7: ' + color + '\n');
+  res.send(JSON.stringify(the_response));
 });
 
 module.exports = router;
